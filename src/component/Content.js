bdac84cb9d4056b90b5bf49dc2a2bde20e5aeb5a
ariedwisountika <ariedwisountika@gmail.com>
2020-12-07 10:29:39 +0700

    tambahdata

--- src/component/Content.js
+++ src/component/Content.js
@@ -1,35 +1,35 @@
-// import React, { Component } from 'react';
-// import {Link} from "react-router-dom";
+import React, { Component } from 'react';
+import {Link} from "react-router-dom";
 
 
-// class Content extends Component {
+class Content extends Component {
 
 
 
     
-//     render(){
-//         return (
-//             <div className="menu-mobile-left-content menu-bg-white">
-//         <ul>
-//           <li><Link to="#"><img src="img/icon_hot_gray.png" alt="Icon Hot Deals" /> <p>Hot Deals</p></Link></li>
-//           <li><Link to="#"><img src="img/icon_food_gray.png" alt="Icon Food" /> <p>Food</p></Link></li>
-//           <li><Link to="#"><img src="img/icon_mobile_gray.png" alt="Icon Mobile & Tablet" /> <p>Mobile &amp; Tablet</p></Link></li>
-//           <li><Link to="#"><img src="img/icon_electric_gray.png" alt="Icon Electric Appliances" /> <p>Electric Appliances</p></Link></li>
-//           <li><Link to="#"><img src="img/icon_computer_gray.png" alt="Icon Electronics & Technology" /> <p>Electronics &amp; Technology</p></Link></li>
-//           <li><Link to="#"><img src="img/icon_fashion_gray.png" alt="Icon Fashion" /> <p>Fashion</p></Link></li>
-//           <li><Link to="#"><img src="img/icon_health_gray.png" alt="Icon Health & Beauty" /> <p>Health &amp; Beauty</p></Link></li>
-//           <li><Link to="#"><img src="img/icon_mother_gray.png" alt="Icon Mother & Baby" /> <p>Mother &amp; Baby</p></Link></li>
-//           <li><Link to="#"><img src="img/icon_book_gray.png" alt="Icon Books & Stationery" /> <p>Books &amp; Stationery</p></Link></li>
-//           <li><Link to="#"><img src="img/icon_home_gray.png" alt="Icon Home & Life" /> <p>Home &amp; Life</p></Link></li>
-//           <li><Link to="#"><img src="img/icon_sport_gray.png" alt="Icon Sports & Outdoors" /> <p>Sports &amp; Outdoors</p></Link></li>
-//           <li><Link to="#"><img src="img/icon_auto_gray.png" alt="Icon Auto & Moto" /> <p>Auto &amp; Moto</p></Link></li>
-//           <li><Link to="#"><img src="img/icon_voucher_gray.png" alt="Icon Voucher Service" /> <p>Voucher Service</p></Link></li>
-//         </ul>
-//       </div>
+    render(){
+        return (
+            <div className="menu-mobile-left-content menu-bg-white">
+        <ul>
+          <li><Link to="#"><img src="img/icon_hot_gray.png" alt="Icon Hot Deals" /> <p>Hot Deals</p></Link></li>
+          <li><Link to="#"><img src="img/icon_food_gray.png" alt="Icon Food" /> <p>Food</p></Link></li>
+          <li><Link to="#"><img src="img/icon_mobile_gray.png" alt="Icon Mobile & Tablet" /> <p>Mobile &amp; Tablet</p></Link></li>
+          <li><Link to="#"><img src="img/icon_electric_gray.png" alt="Icon Electric Appliances" /> <p>Electric Appliances</p></Link></li>
+          <li><Link to="#"><img src="img/icon_computer_gray.png" alt="Icon Electronics & Technology" /> <p>Electronics &amp; Technology</p></Link></li>
+          <li><Link to="#"><img src="img/icon_fashion_gray.png" alt="Icon Fashion" /> <p>Fashion</p></Link></li>
+          <li><Link to="#"><img src="img/icon_health_gray.png" alt="Icon Health & Beauty" /> <p>Health &amp; Beauty</p></Link></li>
+          <li><Link to="#"><img src="img/icon_mother_gray.png" alt="Icon Mother & Baby" /> <p>Mother &amp; Baby</p></Link></li>
+          <li><Link to="#"><img src="img/icon_book_gray.png" alt="Icon Books & Stationery" /> <p>Books &amp; Stationery</p></Link></li>
+          <li><Link to="#"><img src="img/icon_home_gray.png" alt="Icon Home & Life" /> <p>Home &amp; Life</p></Link></li>
+          <li><Link to="#"><img src="img/icon_sport_gray.png" alt="Icon Sports & Outdoors" /> <p>Sports &amp; Outdoors</p></Link></li>
+          <li><Link to="#"><img src="img/icon_auto_gray.png" alt="Icon Auto & Moto" /> <p>Auto &amp; Moto</p></Link></li>
+          <li><Link to="#"><img src="img/icon_voucher_gray.png" alt="Icon Voucher Service" /> <p>Voucher Service</p></Link></li>
+        </ul>
+      </div>
            
-//         );
-//     }
+        );
+    }
 
-// }
+}
 
-// export default Content;
+export default Content;
